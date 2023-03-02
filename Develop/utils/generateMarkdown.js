d9217beee679a520a745cda8958018929fb20279
rambriz91 <rambriz91@gmail.com>
2023-03-02 11:55:01 -0800

    filled out generateMarkdown.js, to export functions to index.js.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,26 +1,31 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
+
+module.exports = [ renderLicenseBadge(), renderLicenseText()]
+
+
+// Render License Badge Function with link
+
 function renderLicenseBadge(license) {
   let badge ='';
-  if (license === MIT) {
+  if (license === 'MIT') {
     badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
   }
-  else if (license === IBM) {
-    badge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/license/ibmpl-php/)'
+  else if (license === 'IBM') {
+    badge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
   }
-  else if (license === ISC) { 
+  else if (license === 'ISC') { 
     badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
   }
-  else if (license === Mozilla) {
+  else if (license === 'Mozilla') {
     badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
   }
   else { 
     badge = ''
   }
-}
+  return badge;
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+//License Text Function
 function renderLicenseText(license) {
   if (license === 'MIT') {
     licenseTxt = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:'
@@ -37,16 +42,3 @@ function renderLicenseText(license) {
     licenseTxt = 'This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0'
   }
 }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -58,7 +58,7 @@ inquirer
     },
     {
         type:'input',
-        message: 'Please fill out the contributing section.',
+        message: 'Please provide instructions on how developers can contribute to your project.',
         name: 'contributing',
     },
     {
@@ -84,54 +84,49 @@ inquirer
     },
 ])
 .then((data) => {
-    data.badge = renderLicenseBadge(data.license),
-    data.licenseTxt = renderLicenseText(data.license),
+    data.badge = generateMarkdown.renderLicenseBadge(data.license),
+    data.licenseTxt = generateMarkdown.renderLicenseText(data.license),
     fs.writeFile('README.md', questions(data), (err) => {
     err ? console.log(err) : console.log('Success!')
     })});
 
-function renderLicenseBadge(license) {
-    let badge ='';
-    if (license === 'MIT') {
-      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-    }
-    else if (license === 'IBM') {
-      badge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
-    }
-    else if (license === 'ISC') { 
-      badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
-    }
-    else if (license === 'Mozilla') {
-      badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-    }
-    else { 
-      badge = ''
-    }
-    return badge;
-  }
+// function renderLicenseBadge(license) {
+//     let badge ='';
+//     if (license === 'MIT') {
+//       badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+//     }
+//     else if (license === 'IBM') {
+//       badge = '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+//     }
+//     else if (license === 'ISC') { 
+//       badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+//     }
+//     else if (license === 'Mozilla') {
+//       badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+//     }
+//     else { 
+//       badge = ''
+//     }
+//     return badge;
+//   }
 
-  function renderLicenseText(license) {
-    if (license === 'MIT') {
-      licenseTxt = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:'
-      'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.'
-      'THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
-    }
-    else if (license === 'IBM') {
-      licenseTxt = 'THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS IBM PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENTS ACCEPTANCE OF THIS AGREEMENT.'
-    }
-    else if (license === 'ISC') {
-      licenseTxt = 'THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.'
-    }
-    else if (license === 'Mozilla') {
-      licenseTxt = 'This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0'
-    }
-    else {
-        licenseTxt = ''
-    }
-    return licenseTxt;
-  }
-
-  //function init() {
-  //  inquirer.prompt(questions)
-  //  .then (answers =>)
- // }
+//   function renderLicenseText(license) {
+//     if (license === 'MIT') {
+//       licenseTxt = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:'
+//       'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.'
+//       'THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
+//     }
+//     else if (license === 'IBM') {
+//       licenseTxt = 'THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS IBM PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENTS ACCEPTANCE OF THIS AGREEMENT.'
+//     }
+//     else if (license === 'ISC') {
+//       licenseTxt = 'THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.'
+//     }
+//     else if (license === 'Mozilla') {
+//       licenseTxt = 'This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0'
+//     }
+//     else {
+//         licenseTxt = ''
+//     }
+//     return licenseTxt;
+//   }
